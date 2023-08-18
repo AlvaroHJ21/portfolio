5dddb01a5cab75608022d29b406cc2b55c41b0e9
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-08-18 18:15:55 -0500

    update: image blur

--- src/components/image/image-blur.tsx
+++ src/components/image/image-blur.tsx
@@ -2,51 +2,18 @@
 
 import clsx from 'clsx';
 import Image from 'next/image';
-import { useState } from 'react';
+import { ComponentProps, useState } from 'react';
 
-interface Props {
-  src: string;
-  alt: string;
-  width?: number;
-  height?: number;
-  className?: string;
-  priority?: boolean;
-}
-
-export const ImageBlur = ({
-  src,
-  alt,
-  width = 800,
-  height = 600,
-  priority = false,
-  className,
-}: Props) => {
+export const ImageBlur = (props: ComponentProps<typeof Image>) => {
   const [isLoading, setLoading] = useState(true);
-  // const [mounted, setMounted] = useState(false);
-
-  // useEffect(() => {
-  //   setMounted(true);
-  // }, []);
-
-  // if (!mounted)
-  //   return <div className={`w-[${width}px] h-[${height}px] bg-gray-200 animate-pulse`}></div>;
-
   return (
     <Image
-      alt={alt}
-      src={src}
-      width={width}
-      height={height}
-      className={clsx(
-        'object-cover',
-        {
-          'scale-110 blur-2xl grayscale': isLoading,
-          'scale-100 blur-0 grayscale-0': !isLoading,
-        },
-        className
-      )}
+      {...props}
+      className={clsx(props.className, 'object-cover', {
+        'scale-110 blur-2xl grayscale': isLoading,
+        'scale-100 blur-0 grayscale-0': !isLoading,
+      })}
       onLoadingComplete={() => setLoading(false)}
-      priority={priority}
     />
   );
 };
