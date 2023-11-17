bbbf284c4b143f5d6943d4f405ab12ea3f3e8997
AlvaroHJ21 <alvarohuaysara@gmail.com>
2023-11-17 15:00:39 -0500

    fix dangerouslySetInnerHTML

--- src/components/organisms/projects/projects-card.tsx
+++ src/components/organisms/projects/projects-card.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
+
+import clsx from 'clsx';
+
 import { Project } from '@/interfaces';
 import { useCursor } from '@/contexts/cursor';
 import { ImageBlur } from '@/components/atoms';
 import { TagGroup } from '@/components/molecules';
-import Link from 'next/link';
-import clsx from 'clsx';
 import { FadeUp } from '@/components/animation/fade-up';
 
 interface Props {
@@ -16,6 +19,14 @@ interface Props {
 export const ProjectsCard = ({ project, large = false }: Props) => {
   const { images, name, categories, tecnologies, description } = project;
   const { setMouseConfig } = useCursor();
+
+  const [isClient, setIsClient] = useState(false);
+
+  // Set isClient to true when component is mounted
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
     <FadeUp className="relative z-10 flex flex-col h-full p-4 rounded-lg dark:border-gray-600 bg-primary dark:bg-background dark:bg-opacity-25 bg-opacity-10 backdrop-blur-sm">
       <Link
@@ -39,10 +50,10 @@ export const ProjectsCard = ({ project, large = false }: Props) => {
         />
       </Link>
       <h3 className="my-2 text-xl">{name}</h3>
-      {large && (
+      {large && description && isClient && (
         <p
           className="hidden mb-4 md:auto md:line-clamp-3"
-          dangerouslySetInnerHTML={{ __html: description ?? '' }}
+          dangerouslySetInnerHTML={{ __html: description }}
         ></p>
       )}
       <div className="h-14">
