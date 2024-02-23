6e22b7d9749693daa55d001fe5b125f137d32bd1
AlvaroHJ21 <alvarohuaysara@gmail.com>
2024-02-23 08:20:41 -0500

    Update link text and size

--- src/components/molecules/link-view-demo.tsx
+++ src/components/molecules/link-view-demo.tsx
@@ -7,12 +7,12 @@ export default function LinkViewDemo({ url }: { url: string }) {
   const { setMouseConfig } = useCursor();
   return (
     <a
-      onMouseOver={() => setMouseConfig({ variant: 'focus-content', content: 'Ver Demo' })}
+      onMouseOver={() => setMouseConfig({ variant: 'focus-content', content: 'Ir' })}
       href={url}
       target="_blank"
       className="absolute flex items-center gap-2 p-2 text-white rounded-md bottom-2 left-2 bg-main hover:opacity-95"
     >
-      <TbExternalLink size={20} /> Ver Demo
+      <TbExternalLink size={24} />
     </a>
   );
 }
